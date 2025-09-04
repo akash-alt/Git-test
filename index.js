89981bc9b3db16638162675e58dae383ab7fc04f
Akash Yadav <akashrequired@gmail.com>
2025-09-04 18:22:19 +0530

    "mathod of array has used in this problem"

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-function outer(){
-    const count = 0;
-    return function inner(){
-        count++;
-        return count;
-    }
-}
-const res = outer();
-console.log(res());
+// Problem statement
+
+// 🔹 Practice Problems (5–7 small ones)
+
+// Print numbers 1–10 using loop.
+
+// Reverse an array without using reverse().
+
+// Find max number in an array.
+
+// Check if a string is a palindrome.
+
+// Count vowels in a string.
+
+// Factorial of a number using loop.
+
+// Sum of even numbers from 1–100.
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
+
+// 1>
+// function printNumbers() {
+//     for(let i=0;i<10;i++){
+//         console.log(i+1);
+//     }   
+// }
+// printNumbers();
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
+// 2>
+// const users = [
+//     {id:1,name:"akash",active:'true'},
+//     {id:2,name:"danni",active:'false'},
+//     {id:3,name:"abc",active:"true"}
+//     ]
+    
+// let res = users.filter((ele) => ele.active).map((ele) => ele.name);
+// console.log(res)
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
+
+
+
